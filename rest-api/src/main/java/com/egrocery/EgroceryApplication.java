865b8a470a5714b9cd294be0694e846fc99d64b8
Rachid BAAZIZ <rachidbaaziz.contact@gmail.com>
2021-02-05 00:09:59 +0100

    [Backend] Generate REST API with Spring boot (#5)
    * [Backend] Generate REST API with Spring boot

---
+++ rest-api/src/main/java/com/egrocery/EgroceryApplication.java
@@ -0,0 +1,13 @@
+package com.egrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgroceryApplication.class, args);
+	}
+
+}
