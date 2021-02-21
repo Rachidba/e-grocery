d3a13c32e4387b3fafd078e0475d1baa5dc8ee50
Rachid BAAZIZ <rachidbaaziz.contact@gmail.com>
2021-02-21 02:05:29 +0100

    [Frontend] Add button to back to products when at Shopping cart

--- web-client/src/components/Navbar/Navbar.tsx
+++ web-client/src/components/Navbar/Navbar.tsx
@@ -5,23 +5,52 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store/reducers';
 import navbarStyle from './NavbarStyle';
 import { Link } from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { useLocation, useHistory } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   const classes = navbarStyle();
+  const location = useLocation();
+  const history = useHistory();
   const numberOfProductInCart: number = useSelector(
     (state: RootState) => state.shoppingCart.shoppingCartItems.length,
   );
+  function renderWithCard() {
+    return (
+      <>
+        <div className={classes.menu}>
+          <MenuIcon color="primary" style={{ fontSize: 35 }} />
+        </div>
+        <Link to={'/shopping-cart'}>
+          <div className={classes.shoppingCart}>
+            <ShoppingCartIcon color="primary" style={{ fontSize: 35 }} />
+            <span className={classes.cartBasket}>{numberOfProductInCart}</span>
+          </div>
+        </Link>
+      </>
+    );
+  }
+  const goToPreviousPath = () => {
+    history.goBack();
+  };
+  function renderWithBackArrow() {
+    return (
+      <IconButton
+        aria-label="back"
+        onClick={goToPreviousPath}
+        className={classes.backText}
+      >
+        <ArrowBackIcon />
+        Produits
+      </IconButton>
+    );
+  }
   return (
     <nav className={classes.navbar}>
-      <div className={classes.menu}>
-        <MenuIcon color="primary" style={{ fontSize: 35 }} />
-      </div>
-      <Link to={'/shopping-cart'}>
-        <div className={classes.shoppingCart}>
-          <ShoppingCartIcon color="primary" style={{ fontSize: 35 }} />
-          <span className={classes.cartBasket}>{numberOfProductInCart}</span>
-        </div>
-      </Link>
+      {location.pathname.includes('cart')
+        ? renderWithBackArrow()
+        : renderWithCard()}
     </nav>
   );
 };
