eb9b55ae5f9126f63b487f60bfe740acfbd2d520
Rachid BAAZIZ <rachidbaaziz.contact@gmail.com>
2021-04-10 05:45:20 +0200

    [Backend] Find nearby shops

--- rest-api/src/main/java/com/egrocery/controllers/ShopController.java
+++ rest-api/src/main/java/com/egrocery/controllers/ShopController.java
@@ -1,13 +1,13 @@
 package com.egrocery.controllers;
 
-import com.egrocery.entities.Product;
-import com.egrocery.repositories.ProductRepository;
+import com.egrocery.entities.Shop;
 import com.egrocery.repositories.ShopRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,9 +22,20 @@ public class ShopController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllShops() {
+    public ResponseEntity<List<Shop>> getAllShops() {
         var shops = shopRepository.findAll();
         var httpStatus = shops.isEmpty() ? HttpStatus.NO_CONTENT : HttpStatus.OK;
         return ResponseEntity.status(httpStatus).body(shops);
     }
+
+    @GetMapping(path = "/nearbyshops")
+    public ResponseEntity<List<Shop>> findShopsByDistanceFromUser(@RequestParam("userlocation") List<Double> userLocation) {
+        double userLongitude = userLocation.get(0);
+        double userLatitude = userLocation.get(1);
+
+        var nearbyShops = shopRepository.findShopsByDistanceFromUser(userLongitude, userLatitude);
+        var httpStatus = nearbyShops.isEmpty() ? HttpStatus.NO_CONTENT : HttpStatus.OK;
+        return ResponseEntity.status(httpStatus).body(nearbyShops);
+    }
+
 }
--- rest-api/src/main/java/com/egrocery/entities/Shop.java
+++ rest-api/src/main/java/com/egrocery/entities/Shop.java
@@ -1,11 +1,17 @@
 package com.egrocery.entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
+import org.n52.jackson.datatype.jts.GeometryDeserializer;
+import org.n52.jackson.datatype.jts.GeometrySerializer;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "shops")
@@ -13,18 +19,26 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Data
 @Builder
-public class Shop {
+public class Shop implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "name", nullable = false)
     private String name;
+
     @Column(name = "description", length = 500)
     private String description;
+
     @Column(name = "img_url")
     private String imgUrl;
-    @Column(name = "location")
-    private String location;
+
+    @JsonSerialize(using = GeometrySerializer.class)
+    @JsonDeserialize(using = GeometryDeserializer.class)
+    private Point geom;
+
     @ManyToOne
     @JoinColumn(name="city_id", nullable=false)
     private City city;
