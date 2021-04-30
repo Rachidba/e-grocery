e852eb016d57e137f33da91a0446b0b348731171
Rachid BAAZIZ <rachidbaaziz.contact@gmail.com>
2021-04-30 23:52:54 +0000

    [Backend] Allow admin to get Sellers list (#30)

--- rest-api/src/main/java/com/egrocery/security/config/WebSecurityConfig.java
+++ rest-api/src/main/java/com/egrocery/security/config/WebSecurityConfig.java
@@ -101,8 +101,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 new AntPathRequestMatcher(SecurityConstants.AUTHENTICATION_URL),
                 new AntPathRequestMatcher(SecurityConstants.CATEGORIES_URL, HttpMethod.GET.toString()),
                 new AntPathRequestMatcher(SecurityConstants.SUBCATEGORIES_URL, HttpMethod.GET.toString()),
-                new AntPathRequestMatcher(SecurityConstants.SHOPS_URL, HttpMethod.GET.toString()),
-                new AntPathRequestMatcher(SecurityConstants.SELLERS_URL, HttpMethod.GET.toString())
+                new AntPathRequestMatcher(SecurityConstants.SHOPS_URL, HttpMethod.GET.toString())
         );
 
         http
@@ -123,6 +122,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, SecurityConstants.ORDER_URL).access("hasRole('" + Role.ROLE_ADMIN.toString() + "')")
                 .antMatchers(HttpMethod.POST, SecurityConstants.ORDER_URL).access("hasRole('" + Role.ROLE_BUYER.toString() + "')")
                 .antMatchers(HttpMethod.POST, SecurityConstants.SELLERS_URL).access("hasRole('" + Role.ROLE_ADMIN.toString() + "')")
+                .antMatchers(HttpMethod.GET, SecurityConstants.SELLERS_URL).access("hasRole('" + Role.ROLE_ADMIN.toString() + "')")
 
                 .and()
                 .authorizeRequests()
