d3162f4921543207b82131097c43a285ec196d7d
Rachid BAAZIZ <rachidbaaziz.contact@gmail.com>
2021-05-02 04:06:16 +0000

    [Frontend][Admin] Allow admin to create sellers accounts

--- admin-web-client/src/components/ShopCreation.tsx
+++ admin-web-client/src/components/ShopCreation.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {
   Create,
   SimpleForm,
+  PasswordInput,
+  NumberInput,
   TextField,
   DateField,
   EditButton,
@@ -12,8 +14,14 @@ const ShopCreation: React.FC = (props) => {
   return (
     <Create title="Create new Shop" {...props}>
       <SimpleForm>
-        <TextInput source="shoneNumber" />
-        <TextInput source="name" />
+        <TextInput source="phoneNumber" />
+        <PasswordInput source="password" />
+        <TextInput source="firstName" />
+        <TextInput source="lastName" />
+        <TextInput source="shopName" />
+        <TextInput source="description" />
+        <NumberInput source="cityId" />
+        <TextInput source="geom" />
       </SimpleForm>
     </Create>
   );
